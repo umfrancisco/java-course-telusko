17f4dee5796eb05991dc3e3b8d60ab5e59e49a3a
umfrancisco <francis.guitler@gmail.com>
2026-05-01 10:29:23 -0300

    update projects

--- section17-project-using-spring-mvc/SpringEcom3/src/main/java/com/umfrancisco/controller/ProductController.java
+++ section17-project-using-spring-mvc/SpringEcom3/src/main/java/com/umfrancisco/controller/ProductController.java
@@ -8,8 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -32,6 +36,17 @@ public class ProductController {
     		return new ResponseEntity<>(product, HttpStatus.OK);
     	} else {
     		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	}
+    }
+    
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+    	Product savedProduct = null;
+    	try {
+    		savedProduct = service.addProduct(product, imageFile);
+    		return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    	} catch (IOException e) {
+    		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     	}
     }
 }
--- section17-project-using-spring-mvc/SpringEcom3/src/main/java/com/umfrancisco/service/ProductService.java
+++ section17-project-using-spring-mvc/SpringEcom3/src/main/java/com/umfrancisco/service/ProductService.java
@@ -4,7 +4,8 @@ import com.umfrancisco.model.Product;
 import com.umfrancisco.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +20,12 @@ public class ProductService {
     
     public Product getProductById(int id) {
     	return repository.findById(id).orElse(new Product(-1));
+    }
+    
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+    	product.setImageName(image.getOriginalFilename());
+    	product.setImageType(image.getContentType());
+    	product.setImageData(image.getBytes());
+    	return repository.save(product);
     }
 }
