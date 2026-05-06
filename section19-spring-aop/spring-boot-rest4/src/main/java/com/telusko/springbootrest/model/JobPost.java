f448ffb819e7daf90691e58560bccc082b9e2c33
umfrancisco <francis.guitler@gmail.com>
2026-05-06 09:24:35 -0300

    update projects

---
+++ section19-spring-aop/spring-boot-rest4/src/main/java/com/telusko/springbootrest/model/JobPost.java
@@ -0,0 +1,63 @@
+package com.telusko.springbootrest.model;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobPost {
+	
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private Integer reqExperience;
+	private List<String> postTechStack;
+	
+	public JobPost() {
+		
+	}
+	
+	public JobPost(int postId, String postProfile, String postDesc, Integer reqExperience, List<String> postTechStack) {
+		this.postId = postId;
+		this.postProfile = postProfile;
+		this.postDesc = postDesc;
+		this.reqExperience = reqExperience;
+		this.postTechStack = postTechStack;
+	}
+	
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	public String getPostProfile() {
+		return postProfile;
+	}
+	public void setPostProfile(String postProfile) {
+		this.postProfile = postProfile;
+	}
+	public String getPostDesc() {
+		return postDesc;
+	}
+	public void setPostDesc(String postDesc) {
+		this.postDesc = postDesc;
+	}
+	public Integer getReqExperience() {
+		return reqExperience;
+	}
+	public void setReqExperience(Integer reqExperience) {
+		this.reqExperience = reqExperience;
+	}
+	public List<String> getPostTechStack() {
+		return postTechStack;
+	}
+	public void setPostTechStack(List<String> postTechStack) {
+		this.postTechStack = postTechStack;
+	}
+	
+	@Override
+	public String toString() {
+		return "JobPost [postId=" + postId + ", postProfile=" + postProfile + ", postDesc=" + postDesc
+				+ ", reqExperience=" + reqExperience + ", postTechStack=" + postTechStack + "]";
+	}
+}
