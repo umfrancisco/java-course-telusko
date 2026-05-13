d1fa87fa68b94fc3c4fd69ae4aa3b795d2539f4e
umfrancisco <francis.guitler@gmail.com>
2026-05-13 08:19:40 -0300

    update projects

--- section20-spring-security/SpringSecurityProject/src/main/java/com/umfrancisco/service/UserService.java
+++ section20-spring-security/SpringSecurityProject/src/main/java/com/umfrancisco/service/UserService.java
@@ -1,5 +1,6 @@
 package com.umfrancisco.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.umfrancisco.model.User;
@@ -9,12 +10,15 @@ import com.umfrancisco.repository.UserRepository;
 public class UserService {
 	
 	private UserRepository repository;
+	private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 	
 	public UserService(UserRepository repository) {
 		this.repository = repository;
 	}
 	
 	public User saveUser(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		System.out.println("Password -> "+user.getPassword());
 		return repository.save(user);
 	}
 }
