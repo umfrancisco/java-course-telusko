5d465ffd482feb76cb76ba6ff7126e3e7eba5eae
umfrancisco <francis.guitler@gmail.com>
2026-05-17 11:28:29 -0300

    update projects

--- section21-jwt/SpringSecurityProject/src/main/java/com/umfrancisco/service/JwtService.java
+++ section21-jwt/SpringSecurityProject/src/main/java/com/umfrancisco/service/JwtService.java
@@ -6,10 +6,12 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -44,10 +46,30 @@ public class JwtService {
 	}
 	
 	public String extractUserName(String token) {
-		return "";
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimResolver.apply(claims);
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.setSigningKey(getKey())
+				.build().parseClaimsJws(token).getBody();
 	}
 	
 	public boolean validateToken(String token, UserDetails userDetails) {
-		return true;
+		final String userName = extractUserName(token);
+		return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+	
+	private Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
 	}
 }
