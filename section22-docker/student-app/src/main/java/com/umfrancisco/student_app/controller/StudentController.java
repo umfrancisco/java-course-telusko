40e0f714a27890f29a2710964ad95d29e46362fa
umfrancisco <francis.guitler@gmail.com>
2026-05-18 21:59:12 -0300

    update projects

---
+++ section22-docker/student-app/src/main/java/com/umfrancisco/student_app/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.umfrancisco.student_app.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.umfrancisco.student_app.model.Student;
+
+@RestController
+public class StudentController {
+	
+	public List<Student> getStudents() {
+		return null;
+	}
+	
+}
