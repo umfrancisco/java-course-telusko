11bfb172e2ded6560ab95481f60b40b29df16aa7
umfrancisco <francis.guitler@gmail.com>
2026-05-26 10:51:19 -0300

    update projects

--- section28-dsa/data-structures-project/src/sorting/Application.java
+++ section28-dsa/data-structures-project/src/sorting/Application.java
@@ -5,26 +5,39 @@ import java.util.Arrays;
 public class Application {
 	
 	public static void main(String[] args) {
-		int[] arr = {7,2,5,1,8,4,5};
-		Algorithms.bubbleSort(arr);
+		int[] arr = {2,5,3,7,1,9};
+		Algorithms.selectionSort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 }
 
 class Algorithms {
 	
+	public static void selectionSort(int[] arr) {
+		int size = arr.length;
+		
+		for (int i = 0; i < size - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < size; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
+		}
+	}
+	
 	public static void bubbleSort(int[] arr) {
-		int steps = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length - 1; j++) {
 				if (arr[j + 1] < arr[j]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
-					steps++;
 				}
 			}
 		}
-		System.out.println("# Number of steps = "+steps);
 	}
 }
--- section28-dsa/data-structures-project/src/sorting/TeluskoResolution.java
+++ section28-dsa/data-structures-project/src/sorting/TeluskoResolution.java
@@ -4,7 +4,35 @@ import java.util.Arrays;
 
 public class TeluskoResolution {
 	
-	public static void main(String[] args) {
+	
+	
+	public static void selectionSort() {
+		int[] nums = {6,5,2,8,9,4};
+		int size = nums.length;
+		int temp = 0;
+		int minIndex = 0;
+		int start = 1;
+		
+		System.out.println("Before sorting: "+Arrays.toString(nums));
+		
+		for (int i = 0; i < size - 1; i++) {
+			minIndex = i;
+			for (int j = start; j < size; j++) {
+				if (nums[minIndex] > nums[j]) {
+					minIndex = j;
+				}
+			}
+			temp = nums[minIndex];
+			nums[minIndex] = nums[i];
+			nums[i] = temp;
+			start++;
+			System.out.println("--> "+Arrays.toString(nums));
+		}
+		
+		System.out.println("After sorting: "+Arrays.toString(nums));
+	}
+	
+	public static void bubbleSort() {
 		int[] nums = {6,5,2,8,9,4};
 		int size = nums.length;
 		int temp = 0;
